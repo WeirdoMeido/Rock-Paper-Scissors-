dbf824fae943e307a9ae5f359f042fe3c908fa49
WeirdoMeido <meiren8adiaz@gmail.com>
2022-07-24 11:54:10 -0500

    Completed game & logic, including scores stopping after reaching 5

--- script.js
+++ script.js
@@ -34,26 +34,55 @@ function playRound (playerSelection) {
     let computerSelection = computerPlay();
     let textDiv = document.querySelector('.parag');
 
+    if (playerScore === 5 || computerScore === 5) {
+        return;
+    }
+
     if (playerSelection.toUpperCase() === computerSelection.toUpperCase()){
         textDiv.textContent = "It's a tie!";
     }
     else if (playerSelection.toUpperCase() === 'ROCK' && computerSelection.toUpperCase() === 'SCISSORS') {
         textDiv.textContent = "You win! Rock beats scissors";
+        playerScore += 1;
+        humScore.textContent = `Player Score : ${playerScore}`;
+        compScore.textContent = `Computer Score : ${computerScore}`;
     }
     else if (playerSelection.toUpperCase() === 'ROCK' && computerSelection.toUpperCase() === 'PAPER') {
         textDiv.textContent = "You lose! Paper beats rock";
+        computerScore += 1;
+        humScore.textContent = `Player Score : ${playerScore}`;
+        compScore.textContent = `Computer Score : ${computerScore}`;
     }
     else if (playerSelection.toUpperCase() === 'PAPER' && computerSelection.toUpperCase() === 'ROCK') {
         textDiv.textContent = "You win! Paper beats rock";
+        playerScore += 1;
+        humScore.textContent = `Player Score : ${playerScore}`;
+        compScore.textContent = `Computer Score : ${computerScore}`;
     }
     else if (playerSelection.toUpperCase() === 'PAPER' && computerSelection.toUpperCase() === 'SCISSORS') {
         textDiv.textContent = "You lose! Scissors beats paper";
+        computerScore += 1;
+        humScore.textContent = `Player Score : ${playerScore}`;
+        compScore.textContent = `Computer Score : ${computerScore}`;
     }
     else if (playerSelection.toUpperCase() === 'SCISSORS' && computerSelection.toUpperCase() === 'PAPER') {
         textDiv.textContent = "You win! Scissors beats paper";
+        playerScore += 1;
+        humScore.textContent = `Player Score : ${playerScore}`;
+        compScore.textContent = `Computer Score : ${computerScore}`;
     }
     else if (playerSelection.toUpperCase() === 'SCISSORS' && computerSelection.toUpperCase() === 'ROCK') {
         textDiv.textContent = "You lose! Rock beats scissors";
+        computerScore += 1;
+        humScore.textContent = `Player Score : ${playerScore}`;
+        compScore.textContent = `Computer Score : ${computerScore}`;
+    }
+
+    if (playerScore === 5) {
+        textDiv.textContent = "Good job! You've won against the computer!";
+    }
+    else if (computerScore === 5) {
+        textDiv.textContent = "Too bad! You've lost against the computer!";
     }
 }
 
